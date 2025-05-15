b519ff5b4f26dbc476d51b32960f5890266c7b30
Martin Pejcinovski <60910022+MartinPej@users.noreply.github.com>
2025-05-16 01:26:13 +1000

    CarCare fix

--- app/src/main/java/com/cse3mad/carcare/MainActivity.kt
+++ app/src/main/java/com/cse3mad/carcare/MainActivity.kt
@@ -42,22 +42,43 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        
+        // Set up bottom navigation with custom navigation handling
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home)
+                    true
+                }
+                R.id.navigation_mechanic -> {
+                    navController.navigate(R.id.navigation_mechanic)
+                    true
+                }
+                R.id.navigation_guides -> {
+                    navController.navigate(R.id.navigation_guides)
+                    true
+                }
+                R.id.navigation_my_car -> {
+                    navController.navigate(R.id.navigation_my_car)
+                    true
+                }
+                else -> false
+            }
+        }
 
         // Add destination change listener
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.navigation_home,
-                R.id.navigation_mechanic,
-                R.id.navigation_guides,
-                R.id.navigation_my_car -> {
-                    navView.menu.findItem(destination.id)?.isChecked = true
-                }
-                R.id.myCarDashboardFragment,
-                R.id.carDetailsFormFragment,
-                R.id.carDisplayFragment -> {
-                    // These are part of the My Car flow, so select the My Car tab
-                    navView.menu.findItem(R.id.navigation_my_car)?.isChecked = true
+                R.id.navigation_home -> navView.menu.findItem(R.id.navigation_home)?.isChecked = true
+                R.id.navigation_mechanic -> navView.menu.findItem(R.id.navigation_mechanic)?.isChecked = true
+                R.id.navigation_guides -> navView.menu.findItem(R.id.navigation_guides)?.isChecked = true
+                R.id.navigation_my_car -> navView.menu.findItem(R.id.navigation_my_car)?.isChecked = true
+                else -> {
+                    // For other destinations, find the parent destination and select it
+                    val parentDestination = navController.graph.findNode(destination.id)?.parent?.id
+                    if (parentDestination != null) {
+                        navView.menu.findItem(parentDestination)?.isChecked = true
+                    }
                 }
             }
         }
